2a2c207fa275ded09080fdcf83516c20391e360c
zhangyi2k15 <zhangyi2k15@163.com>
2019-03-16 12:11:53 +0800

    Initial commit the pratice file

---
+++ pratice.py
@@ -0,0 +1,18 @@
+class Cat:
+    def __init__(self):
+        """
+        看炒股软件
+        """
+        pass
+    def eat(self):
+        print("吃")
+
+    def drink(self):
+        print('喝水')
+
+
+tom = Cat()
+tom.drink()
+tom.eat()
+
+
