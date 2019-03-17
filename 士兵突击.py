67a591057391366e4feff8ce12e4904724d9d305
zhangyi2k15 <zhangyi2k15@163.com>
2019-03-17 11:20:32 +0800

    this is the object pratice file and update by coach zhang.
    the second commit of coach zhang.

--- 士兵突击.py
+++ 士兵突击.py
@@ -10,7 +10,7 @@ class Gun:
 
     def shoot(self):
         if self.bullet_count <= 0:
-            print("%s 没有子弹了...." % (self.model))
+            print("%s 没有子弹了...." % self.model)
 
             return
 
@@ -19,6 +19,20 @@ class Gun:
         print("%s 发射了一枚子弹...，剩余 %d" % (self.model, self.bullet_count))
 
 
+class Soldier:
+
+    def __init__(self, name):
+
+        self.name = name
+        self.gun = None
+
+
 ak47 = Gun('AK47')
 ak47.add_bullet(100)
 ak47.shoot()
+ak47.shoot()
+
+xusanduo = Soldier('许三多')
+xusanduo.gun = ak47
+print(xusanduo.gun)
+
